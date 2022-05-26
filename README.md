6ff5ee047130b55f4d177a1f2184b3bafb56fb54
SU PAN <liulaisusan@gmail.com>
2022-05-26 14:52:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tutorial
+This folder is the code from tutorial https://www.youtube.com/watch?v=W6NZfCO5SIk&ab_channel=ProgrammingwithMosh 
---
+++ index.js
@@ -0,0 +1,28 @@
+let user = 'Mosh'; // using let to declare a variable
+console.log(user);
+
+// cannot bea reserverd keyword
+// should be meaningful
+// cannot start with a number
+// cannot contain a space of hyphen, using carmel notation
+// are case senstitive
+
+let firstName = 'Su';
+let lastName = 'Pan';
+let age = 27; // no floating or integer
+let isApproved = true;
+let future = undefined; // underfined is a type and a value
+let past = null; // to clear the value of a variable, type of this is an object
+
+
+const interestRate = 0.3;
+console.log(interestRate);
+
+// js is dynamic programming, the type of the variable can be changed
+
+// reference types: object, array , function
+
+// object
+let person = {
+    user: 'Su',
+};
